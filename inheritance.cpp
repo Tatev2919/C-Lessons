0430f0968f994f26434587b52ec07159486a998a
Tatev2919 <tatev@karapetian.ru>
2022-06-25 20:55:56 +0400

    inher updated

--- inheritance.cpp
+++ inheritance.cpp
@@ -5,6 +5,7 @@ class Person{
 	string name, surname;
 	int age;
 	void print (){
+		cout << "Person print" << endl; 
 		cout << "name is : " << name << endl;
 		cout << "surname is : " << surname << endl;
 		cout << "age is : " << age << endl;
@@ -13,27 +14,80 @@ class Person{
 };
 
 class specialist : public Person {
-	int IQ;
-	int languages;
+	public:
+		int IQ;
+		int languages;
+	void print (){
+		cout << "specialist print" << endl; 
+		cout << "name is : " << name << endl;
+		cout << "surname is : " << surname << endl;
+		cout << "age is : " << age << endl;
+		cout << "IQ is : " << IQ << endl;
+		cout << "languages is : " << languages << endl;
+	}
 };
 
-class Medic : public specialist {
-
+class medic : public specialist {
+	public:
+		string experience;
+		string salary;
+	void print (){
+		cout << "specialist print" << endl; 
+		cout << "name is : " << name << endl;
+		cout << "surname is : " << surname << endl;
+		cout << "age is : " << age << endl;
+		cout << "IQ is : " << IQ << endl;
+		cout << "languages is : " << languages << endl;
+		cout << "exp is : " << experience << endl;
+		cout << "salary is : " << salary << endl;
+	}
 
 };
 
-class surgeon : public specialist {
+class surgeon : public medic {
+	public:
+		string direction;
+	void print (){
+		cout << "specialist print" << endl; 
+		cout << "name is : " << name << endl;
+		cout << "surname is : " << surname << endl;
+		cout << "age is : " << age << endl;
+		cout << "IQ is : " << IQ << endl;
+		cout << "languages is : " << languages << endl;
+		cout << "exp is : " << experience << endl;
+		cout << "salary is : " << salary << endl;
+		cout << "dir is : " << direction << endl;
+	}
 
 };
-class cardio_surgeon : public specialist {
+class cardio_surgeon : public surgeon {
+	public:
+		int patients;
+	void print (){
+		cout << "specialist print" << endl; 
+		cout << "name is : " << name << endl;
+		cout << "surname is : " << surname << endl;
+		cout << "age is : " << age << endl;
+		cout << "IQ is : " << IQ << endl;
+		cout << "languages is : " << languages << endl;
+		cout << "exp is : " << experience << endl;
+		cout << "salary is : " << salary << endl;
+		cout << "patients is : " << patients << endl;
+	}
 
 };
 
 int main() {
-	Person p;
-	p.name = "T";
-	p.surname = "K";
-	p.age = 15;
-	p.print();
+	cardio_surgeon c;
+	c.name = "T";
+	c.surname = "K";
+	c.age = 15;
+	c.IQ = 150;
+	c.languages = 3;
+	c.experience = "5";
+        c.salary = "1.5b";
+	c.direction = "cardio";
+	c.patients = 2500;	
+	c.print();
 	return 0;
 }
