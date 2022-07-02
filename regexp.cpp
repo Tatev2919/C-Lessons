f9d23b2d56af56595312fdf778f550ca0411990d
Tatev2919 <tatev@karapetian.ru>
2022-07-02 20:37:39 +0400

    IPv6 optimized version and worked name part.

--- regexp.cpp
+++ regexp.cpp
@@ -18,7 +18,7 @@ int main(){
 	regex ucom("(\\(?0(55|95)\\)?|\\+(\\(?374\\)?)\\s?(55|95))\\s?([0-9]{2}[-\\s]?){2}[0-9]{2}");
 	regex passport("[A][A-Z][0-9]{7}");
 	regex date("(([0-2][0-9])|(3[0-1]))[\\/\\.\\-]((0[0-9])|(1[0-2]))[\\/\\.\\-][0-2][0-9]{3}");
-	regex name ("\\b([A-Z][a-z]{1,}\\s[A-Z][a-z]{1,}(ian|yan|uni|enc|deh|toc))\\b");
+	regex name ("\\b([A-Z][a-z]{1,}\\s(?!Mkh)[A-Z][a-z]{1,}(ian|yan|uni|enc|deh|toc))\\b");
 	regex ipv4("(((25[0-5])|(2[0-4][0-9])|(1[0-9]{2})|([1-9][0-9])|([0-9]))\\.){3}((25[0-5])|(2[0-4][0-9])|(1[0-9]{2})|([1-9][0-9])|([0-9]))");
 	regex ipv6("((([1-9A-F][0-9A-F]{2,3})|([1-9A-F][0-9A-F])|([0-9A-F]))\\:){7}(([1-9A-F][0-9A-F]{2,3})|([1-9A-F][0-9A-F])|([0-9A-F]))",regex_constants::icase);
 	
@@ -29,7 +29,7 @@ int main(){
 	string name_t = "Yeghishe Charenc is a famous writer and Henrikh Mkhitaryan is a famous foolball player.";
 	string date_t = "Jane birthday is on 29.07.1998 and Jane brother's birthday is on 22-11-1995 or 31.15.2015";
 	string ip4_t = "IPv4 form is : 0.255.15.0 or 15.155.49.9 or 255.255.255.249 but not 15..45.47.49 or 99.65.41 or 15.15 or 12";
-	string ip6_t = "IPv6 form is : 0:2F:AB:570:4F:1a:FFFF:78 or 15:FFFa:2a:97B:1a:48D:47:255 or 25a:2f5:2:147:444a:5D:47c:12a but not 15::4b5:47a:49 or 919:ff:m or a:0 or 0";
+	string ip6_t = "IPv6 form is : 0FFF:2F:AB:570:4F:1a:FFFF:78 or 15:FFFa:2a:97B:1a:48D:47:255 or 25a:2f5:2:147:444a:5D:47c:12a but not 15::4b5:47a:49 or 919:ff:m or a:0 or 0";
 	
 	cout << "---------"<< endl;	
 	searcher(viv_t,vivacell);	
