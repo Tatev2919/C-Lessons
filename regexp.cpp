e20f4b2beaad5be749e58eaf1fc9005b7d47e812
Tatev2919 <tatev@karapetian.ru>
2022-07-02 18:33:23 +0400

    Regexp simple examples:

--- regexp.cpp
+++ regexp.cpp
@@ -10,6 +10,7 @@ int main(){
 	regex date("(([0-2][0-9])|(3[0-1]))[\\/\\.\\-]((0[0-9])|(1[0-2]))[\\/\\.\\-][0-2][0-9]{3}");
 	string text1 = "Phone numbers people display in different ways: 094 54 54 54 or +37494545454 you can also use +(374)94 54-54-54 this form or (094)54-54-54. But it's also accepted way to use this form  (094) 54 54 54 and  +374 94 54 54 54.";
 	string text = "Passport seria: AN1444442 .";
+	string name_surname = "[A-Z][a-z]{3,}\\s[A-Z][a-z]{2,}(ian|yan|uni|enc|deh|toc)";
 	string d = "Jane birthday is on 29.07.1998 and Jane brother's birthday is on 22-11-1995 or 31.15.2015";
 	smatch cor;
 	
