7ee5d6d51fdcbe6102749078fac1319a1b283d9b
Tatev2919 <tatev@karapetian.ru>
2022-07-26 23:11:18 +0400

    map updated version

--- templates/map.cpp
+++ templates/map.cpp
@@ -17,7 +17,24 @@ class Specialist {
 		this->languages = languages;
 		this->iq = iq;
 	}
-	
+   
+	//operator bool(){
+	//	if (name !=nullptr || surname !=nullptr || age!=0) {
+	//		return true;
+	//	} else {
+	//		return false;
+	//	}
+	//}
+	friend ostream& operator<<(ostream& pr, Specialist spec){
+                cout<<"---operator<< call for Specialist object---"<<endl;
+                pr<<spec.name<<endl;
+                pr<<spec.surname<<endl;
+                pr<<spec.age<<endl;
+                pr<<spec.languages<<endl;
+                pr<<spec.iq<<endl;
+                return pr;
+        }	
+
 	void print() {
 		cout << name << " : name " << endl;
 		cout << surname << " : surname" << endl;
@@ -28,9 +45,11 @@ class Specialist {
 };
 
 template <class T> 
-class Comparator <T> {
+class Comparator {
         public:
-		virtual int comp(T a, T b) {};
+		virtual int comp(T a, T b) {
+			return -77;
+		};
 };
 
 class SortByAge: public Comparator<Specialist> {
@@ -68,7 +87,7 @@ class Map{
     private:
         Node<K,V>* root = nullptr;
         int length = 0;
-        Comparator* p;
+        Comparator<K>* p;
 
     public:
 		bool is_empty(){
@@ -81,12 +100,13 @@ class Map{
 			}				
 		}
 
-		Map (Comparator &p) {
+		Map (Comparator<K>* p) {
 			this->p = p;
 		}
 		
 		Map (const Map& t ) {
 			cout << "called copy constructor" << endl; 
+                        this->p = t->p; 
 			cp(t.root);       	
 		}
 	
@@ -98,8 +118,6 @@ class Map{
 			if (!is_empty()){
 				destroy(root);
 			}
-			//Comment special:
-			//root link is not nullpointer if the line below wasn't written
 			root = nullptr;
 			cp(t.root);
 			return* this;       	
@@ -128,16 +146,11 @@ class Map{
 		}
 		
 
-		//void print(){
-		//	if (!is_empty()) {
-		//		print(root);
-		//	}
-		//}
-		//void print(bool desc) {
-		//	if (!is_empty()) {
-		//		print(root,desc);
-		//	}
-		//}
+		void print(){
+			if (!is_empty()) {
+				print(root);
+			}
+		}
 
 		bool find(K key){
 			if (!is_empty()) {
@@ -172,7 +185,7 @@ class Map{
 			//case 1: to be deleted node has no leaves
 			if (tmp->left == nullptr && tmp->right == nullptr){
 			 	auto t = tmp->parent;	
-				if (p::comp(t->key,tmp->key)==1) {
+				if (p->comp(t->key,tmp->key)==1) {
 					f = 1;
 				} 
 				delete tmp;
@@ -269,7 +282,7 @@ class Map{
 				return false;
 			}
 			if (tmp1!= nullptr && tmp2!=nullptr){
-				if((p::comp(tmp1->key,tmp2->key)==-1) || (p::comp(tmp1->value,tmp2->value)==-1)) {
+				if((p->comp(tmp1->key,tmp2->key)==-1) || (tmp1->value < tmp2->value)) {
 				//	cout << "Second if" << endl;
 				//	cout << tmp1->key << " tmp1->key and tmp2->key " <<  tmp2->key  << endl;
 				//	cout << tmp1->value << " tmp1->value and tmp2->value " <<  tmp2->value << endl;
@@ -286,35 +299,13 @@ class Map{
 
 
 		//Display binary search tree with ascending order
-		//void print(Node<K,V>* tmp) {
-		//	if (tmp != nullptr) { 
-		//		print(tmp->left);
-		//		cout << "key: " << tmp->key << " value: " <<  tmp->value << endl;
-		//		print(tmp->right);
-		//	} 
-		//}
-		////Display binary search tree with descending order	
-	
-		//void print(Node<K,V>* tmp, bool desc) {
-		//	if (tmp != nullptr) { 
-		//		print(tmp->right,true);
-		//		cout << "key: " << tmp->key << " value: " <<  tmp->value << endl;
-		//		print(tmp->left,true);
-		//	} 
-		//}
-		
-		//int find_loc(K key) {
-		//	int i = 0;
-		//	for (Node<K,V>* tmp = head; tmp != nullptr; tmp = tmp->next) {
-		//	    if (key > tmp->key) {
-		//		insert(t, i);
-		//		//return;
-		//	    }
-		//	    ++i;
-		//	}
-
-		//}
-		
+		void print(Node<K,V>* tmp) {
+			if (tmp != nullptr) { 
+				print(tmp->left);
+				cout << "key: " << tmp->key << "value: " <<  tmp->value << endl;
+				print(tmp->right);
+			} 
+		}
 
 		void add(Node<K,V>* tmp, V value,K key) {
 			//Comment special:
@@ -328,12 +319,10 @@ class Map{
 			}
 			auto t = find_ex(key); 
 			if (t != nullptr) {
-				while(key!=nullptr ) {
 				t->value = value;
 				return;
-				}
 			}
-			if ( (p::comp(key,tmp->key))==1 ) {
+			if ( (p->comp(key,tmp->key))==1 ) {
 				//Comment special:
 				//in this case tmp->value is not in managed address
 				if (tmp->right == nullptr) {	
@@ -345,7 +334,7 @@ class Map{
 				} else {
 					add(tmp->right, value, key);
 				}
-			} else if ( p::comp(key,tmp->key)==-1) {
+			} else if ( p->comp(key,tmp->key)==-1) {
 				if (tmp->left == nullptr) {
 					//cout << find_ex(key) << " key " << key << endl;
 					tmp->left=new Node<K,V>(key,value);
@@ -363,7 +352,7 @@ class Map{
 			} 
 			if (key==tmp->key) {
 				return true;
-			} else if ((p::comp(key,tmp->key) == 1) ) {
+			} else if ((p->comp(key,tmp->key) == 1) ) {
 				return find(tmp->right, key);
 			} else {
 				return find(tmp->left, key);
@@ -376,10 +365,10 @@ class Map{
 			if (tmp==nullptr) {
 				return nullptr;
 			}
-			if ( p::comp(key,tmp->key)==0) {
+			if ( p->comp(key,tmp->key)==0) {
 				cout << "hello"<<endl;
 				return tmp;
-			} else if ( p::comp(key,tmp->key)==1 ) {
+			} else if ( p->comp(key,tmp->key)==1 ) {
 				return find_ex(tmp->right, key);
 			} else {
 				return find_ex(tmp->left, key);
@@ -400,6 +389,12 @@ int main () {
 	b.add(3,Specialist("Kareem","S",35,3,120));
 	b.add(4,Specialist("Nahlu","Mohammad",12,1,75));
 	b.print();
+	Map<Specialist,int> b1(&s);
+	b1.add(1,Specialist("John","Walker",15,2,90));
+	b1.add(2,Specialist("Jane","Smith",18,2,95));
+	b1.add(3,Specialist("Kareem","S",35,3,120));
+	b1.add(4,Specialist("Nahlu","Mohammad",12,1,75));
+	b1.print();
 	//Map<int,int> b1;
 	//b1.add(5,1);
         //b1.add(3,2);
@@ -413,34 +408,35 @@ int main () {
 	//b.print();
 	//cout << "----------------Print in desc order-------------" << endl;
 	//b.print(true);
-	//b1 = b;
-	//cout << "------------print copy-------------" << endl;
-	//b1.print();
-	//cout << "---------------calling copy constructor-----------------" << endl;
+        Map<Specialist,int> b2(&s);
+	b2 = b;
+	cout << "------------print copy-------------" << endl;
+	b2.print();
+	cout << "---------------calling copy constructor-----------------" << endl;
 	//Map<int,int> b2 = b1;
 	//b2.print();
 	//cout << "---------------Compare Map s-----------------" << endl;
-	//if (b2==b1) {
-	//	cout << "Map s are equal" << endl;
-	//} else {
-	//	cout << "Map s aren't equal" << endl;
-	//}
-	//cout << "-----------------Removing part---------------" << endl;
-	//cout << "Removing key 4 : case 1 has no leaves" << endl;
-	//b.print();
-	//b.remove(4);
-	//cout << "After removing key 4 value 65 " << endl;
-	//b.print();
-	//cout << "-----------------Removing part---------------" << endl;
-	//cout << "Removing key 0 : case 2 has one leaf" << endl;
-	//b.print();
-	//b.remove(0);
-	//cout << "After removing key 0 value 42 " << endl;
-	//b.print();
-	//cout << "-----------------Removing part---------------" << endl;
-	//cout << "Removing key 6 : case 3 has 2 leaves" << endl;
-	//b.print();
-	//b.remove(6);
+	if (b==b1) {
+		cout << "Map s are equal" << endl;
+	} else {
+		cout << "Map s aren't equal" << endl;
+	}
+	cout << "-----------------Removing part---------------" << endl;
+	cout << "Removing key 4 : case 1 has no leaves" << endl;
+	b.print();
+	b.remove(Specialist("John","Walker",15,2,90));
+//	cout << "After removing key 4 value 65 " << endl;
+//	b.print();
+//	cout << "-----------------Removing part---------------" << endl;
+//	cout << "Removing key 0 : case 2 has one leaf" << endl;
+//	b.print();
+//	b.remove(0);
+//	cout << "After removing key 0 value 42 " << endl;
+//	b.print();
+//	cout << "-----------------Removing part---------------" << endl;
+//	cout << "Removing key 6 : case 3 has 2 leaves" << endl;
+//	b.print();
+//	b.remove(6);
 	//cout << "After removing key 6 value 100 " << endl;
 	//b.print();
  
