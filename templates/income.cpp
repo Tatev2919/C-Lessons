90fe27b2cf367ac25516cf5b113371878598a635
Tatev2919 <tatev@karapetian.ru>
2022-07-30 20:39:49 +0400

    income final

--- templates/income.cpp
+++ templates/income.cpp
@@ -25,7 +25,7 @@ class Income{
                 this->fromEducation= t.fromEducation;
 		this->fromConsulting = t.fromConsulting;
 	}
-	double sum() {
+	double sum() const{
 		return (this->fromShop+this->fromTenders+this->fromApps+this->fromEducation+this->fromConsulting);
 	}
 
@@ -40,6 +40,119 @@ class Income{
 	bool operator>(const Income& t) {
 		return (this->sum() > t.sum()); 
 	}
+	
+	bool operator<(const Income& t) {
+		return (this->sum() < t.sum()); 
+	}
+
+	bool operator>=(const Income& t) {
+		return (this->sum() >= t.sum()); 
+	}
+
+	bool operator<=(const Income& t) {
+		return (this->sum() <= t.sum()); 
+	}
+	
+	const Income operator*(const Income& t) {
+		Income temp;
+		temp.fromShop = this->fromShop * t.fromShop;
+		temp.fromTenders = this->fromTenders * t.fromTenders;
+		temp.fromApps = this->fromApps * t.fromApps;
+                temp.fromEducation= this->fromEducation * t.fromEducation;
+		temp.fromConsulting = this->fromConsulting * t.fromConsulting;
+		
+		return temp; 
+	}
+	const Income operator*=(const Income& t) {
+	//	return (*this+t);
+		this->fromShop *= t.fromShop;
+		this->fromTenders *= t.fromTenders;
+		this->fromApps *= t.fromApps;
+                this->fromEducation *= t.fromEducation;
+		this->fromConsulting *= t.fromConsulting;
+		return *this;
+	}
+
+	const Income operator/(const Income& t) {
+		Income temp;
+		temp.fromShop = this->fromShop / t.fromShop;
+		temp.fromTenders = this->fromTenders / t.fromTenders;
+		temp.fromApps = this->fromApps / t.fromApps;
+                temp.fromEducation= this->fromEducation / t.fromEducation;
+		temp.fromConsulting = this->fromConsulting / t.fromConsulting;
+		
+		return temp; 
+	}
+	const Income operator/=(const Income& t) {
+	//	return (*this+t);
+		this->fromShop /= t.fromShop;
+		this->fromTenders /= t.fromTenders;
+		this->fromApps /= t.fromApps;
+                this->fromEducation /= t.fromEducation;
+		this->fromConsulting /= t.fromConsulting;
+		return *this;
+	}
+	const Income operator+(const Income& t) {
+                cout << "hello" << endl;
+		Income temp;
+		temp.fromShop = this->fromShop + t.fromShop;
+		temp.fromTenders = this->fromTenders + t.fromTenders;
+		temp.fromApps = this->fromApps + t.fromApps;
+                temp.fromEducation= this->fromEducation + t.fromEducation;
+		temp.fromConsulting = this->fromConsulting + t.fromConsulting;
+		
+		return temp; 
+	}
+	const Income operator+=(const Income& t) {
+	//	return (*this+t);
+		this->fromShop += t.fromShop;
+		this->fromTenders += t.fromTenders;
+		this->fromApps += t.fromApps;
+                this->fromEducation += t.fromEducation;
+		this->fromConsulting += t.fromConsulting;
+		return *this;
+	}
+	const Income operator++() {
+	//	return (*this+t);
+		this->fromShop++;
+		this->fromTenders++;
+		this->fromApps++;
+                this->fromEducation++;
+		this->fromConsulting++;
+		return *this;
+	}
+	const Income operator++( int ch) {
+	//	return (*this+t);
+                Income temp = *this;
+		this->fromShop++;
+		this->fromTenders++;
+		this->fromApps++;
+                this->fromEducation++;
+		this->fromConsulting++;
+                //temp.print();
+		//cout<< "Temp is ----------" <<endl;
+		return temp;
+	}
+	const Income operator-(const Income& t) {
+		Income temp;
+		temp.fromShop = this->fromShop - t.fromShop;
+		temp.fromTenders = this->fromTenders - t.fromTenders;
+		temp.fromApps = this->fromApps - t.fromApps;
+                temp.fromEducation= this->fromEducation - t.fromEducation;
+		temp.fromConsulting = this->fromConsulting - t.fromConsulting;
+		
+		return temp; 
+	}
+	const Income operator-=(const Income& t) {
+	//	return (*this-t);
+		this->fromShop -= t.fromShop;
+		this->fromTenders -= t.fromTenders;
+		this->fromApps -= t.fromApps;
+                this->fromEducation -= t.fromEducation;
+		this->fromConsulting -= t.fromConsulting;
+		return *this;
+	}
+
 	void print () {
 		cout <<"fromShop: " << fromShop << endl;
 		cout << "fromTenders: " << fromTenders<<endl;
@@ -70,5 +183,30 @@ int main() {
 	} else {
 		cout << "i1 more than i" << endl;
 	}
+	cout << "Mult value is :" << endl;
+	Income k = i * i1;
+	k.print();
+	cout << "------------------- += value is :" << endl;
+	i+=i1;
+	i.print();
+	cout << "-------------------- *= value is :" << endl;
+	i*=i1;
+	i.print();
+	cout << "---------------------- /= value is :" << endl;
+	i/=i1;
+	i.print();
+	cout << "------------------------ -= value is :" << endl;
+	i-=i1;
+	i.print();
+	cout << "------------------------ prefix ++ value is :" << endl;
+	Income ch = i++;
+	ch.print();
+        cout << "------------------------------------- i value is : " << endl;
+	i.print();
+	cout << "------------------------ postfix ++ value is :" << endl;
+	Income m = ++i;
+	m.print();
+        cout << "---------------------------------- original value is : " << endl;
+	i.print();
 	return 0;
 }
