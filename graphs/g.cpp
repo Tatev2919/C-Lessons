bc2bff6ac460f42e6ca41a0e03513bc32c60bf9a
Tatev2919 <tatev@karapetian.ru>
2022-08-23 21:57:16 +0400

    h

--- graphs/g.cpp
+++ graphs/g.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main() {
-	int** x = new int*[11];
+	int** x = new int*[9];
         
-        for(int i = 2; i < 11; i++){
-        	x[i] = new int[11];
-                for(int j = 0; j < 11; j++){
+        for(int i = 0; i < 9; i++){
+        	x[i] = new int[9];
+                for(int j = 0; j < 9; j++){
                 	x[i][j] = 0;
                 }
 	}
@@ -23,28 +23,37 @@ int main() {
         x[6][3] = 1;
         x[6][5] = 1;
         x[6][7] = 1;
-        x[6][9] = 1;
+        x[6][1] = 1;
         x[7][4] = 1;
         x[7][6] = 1;
-        x[7][10] = 1;
+        x[7][0] = 1;
         x[8][4] = 1;
-        x[8][9] = 1;
-        x[9][6] = 1;
-        x[9][8] = 1;
-        x[9][10] = 1;
-        x[10][7] = 1;
-        x[10][9] = 1;
+        x[8][1] = 1;
+        x[1][6] = 1;
+        x[1][8] = 1;
+        x[1][0] = 1;
+        x[0][7] = 1;
+        x[0][1] = 1;
         
-        for(int i = 2; i < 11; i++){
-                for(int j = 2; j < 11; j++){
+        string names [9] = {"Yerevan","Ashtarak","Gyumri","Artashat","Yeghvard","Talin", "Aparan", "Abovyan","Oshakan"}; 
+        for(int i = 0; i < 9; i++){
+                for(int j = 0; j < 9; j++){
                 	cout << x[i][j] << "  ";
                 } 
                 cout << endl;
 	}
+        for(int i = 0; i < 9; i++){
+                for(int j = 0; j < 9; j++){
+                	//cout << x[i][j] << "  ";
+                        if (x[i][j]==1) {
+                	     cout << names[i] << " ---- > " << names[j]<<endl;
+                        }
+                } 
+                cout << endl;
+	}
         
-        string names [11] = {"-","-","Gyumri","Artashat","Yeghvard","Talin", "Aparan", "Abovyan","Oshakan","Ashtarak", "Yerevan"}; 
         
-        for(int i = 2; i < 11; i++){
+        for(int i = 0; i < 9; i++){
                 cout << i <<" : " << names[i] << endl;
         }
 	return 0;
